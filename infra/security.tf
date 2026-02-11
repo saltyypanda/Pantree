ea8447aa78458c95650abf692d6a6838b40e90ff
saltyypanda <esh7943@g.rit.edu>
2026-02-10 22:41:29 -0500

    Added secrets manager vpc endpoint

--- infra/security.tf
+++ infra/security.tf
@@ -72,3 +72,37 @@ resource "aws_lambda_permission" "allow_apigw_invoke_me" {
 
   source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
+
+# -----------------------
+# VPC resources
+# -----------------------
+
+resource "aws_security_group" "vpce" {
+  name   = "${var.name}-vpce-sg"
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  ingress {
+    from_port                = 443
+    to_port                  = 443
+    protocol                 = "tcp"
+    security_groups = [ aws_security_group.lambda.id ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id              = data.aws_vpc.default_vpc.id
+  vpc_endpoint_type   = "Interface"
+  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+
+  subnet_ids          = aws_db_subnet_group.db.subnet_ids
+  security_group_ids  = [aws_security_group.vpce.id]
+
+  private_dns_enabled = true
+}
